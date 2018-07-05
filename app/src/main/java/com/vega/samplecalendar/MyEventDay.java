869ed2eb98d356231f2d43327e39995a6a677227
Ronald Vega <ronald.vega48@gmail.com>
2018-07-05 14:03:11 -0600

    Mostrando Calendario

---
+++ app/src/main/java/com/vega/samplecalendar/MyEventDay.java
@@ -0,0 +1,49 @@
+package com.vega.samplecalendar;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import com.applandeo.materialcalendarview.EventDay;
+
+import java.util.Calendar;
+
+public class MyEventDay extends EventDay implements Parcelable {
+
+    private String mNote;
+    public MyEventDay(Calendar day, int imageResource, String mNote) {
+        super(day, imageResource);
+        this.mNote = mNote;
+    }
+
+    public String getmNote() {
+        return mNote;
+    }
+
+    private MyEventDay(Parcel in) {
+        super((Calendar) in.readSerializable(), in.readInt());
+        mNote = in.readString();
+    }
+
+    public static final Creator<MyEventDay> CREATOR = new Creator<MyEventDay>() {
+        @Override
+        public MyEventDay createFromParcel(Parcel in) {
+            return new MyEventDay(in);
+        }
+        @Override
+        public MyEventDay[] newArray(int size) {
+            return new MyEventDay[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeSerializable(getCalendar());
+        dest.writeInt(getImageResource());
+        dest.writeString(mNote);
+    }
+}
